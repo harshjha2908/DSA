754e07adb6ba3cddfbfb6d9900cd99e7407f61b8
Harsh Kumar Jha <harhkrjha.2908@gmail.com>
2026-06-30 18:44:36 +0530

    minor error

---
+++ sumandduplicate.java
@@ -0,0 +1,29 @@
+public class sumandduplicate {
+    public static void main(String[] args) {
+        int arr[]={1 , 1 ,1 ,2 ,2 ,3 ,3 ,4 ,4 ,4 ,5 ,5};
+        int target=7;
+    
+        int i=0;
+
+        int j=arr.length-1;
+        while (i<j) {int sum = arr[i]+arr[j];
+            if (sum==target){
+                System.out.println(arr[i]+" "+arr[j]);
+                while (arr[i]==arr[i+1]) {i++;
+                    
+                }
+                while (arr[j]==arr[j-1]) {j--;
+                    
+                }
+
+            }else if (sum>target) {j--;
+                
+            }else{
+                i++;
+            }
+            
+            
+        }
+
+    }
+}
